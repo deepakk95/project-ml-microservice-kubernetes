d6fd6c1f520cd1d4359bede672be534305655467
Deepak Kataria <deepak.kataria@olx.com>
2022-08-15 00:42:53 +0530

    Changes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="deepakk95/mlservice"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlservice\
+    --image=$dockerpath\
+    --port=80 --labels app=mlservice
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward mlservice 8000:80
 
