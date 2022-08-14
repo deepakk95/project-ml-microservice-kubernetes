719ff5d1e5c93b6e4d6f52f83f336bce3dbf2a99
EC2 Default User <ec2-user@ip-172-31-52-250.ec2.internal>
2022-08-14 18:32:31 +0000

    Changes

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="deepakk95/mlservice"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag mlservice $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
